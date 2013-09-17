d6708f523abd12d62b22b507e8fd7fd1f9eaf0ec
ibbolia <mwfitzgibbon@wpi.edu>
2013-09-17 09:26:46 -0400

    First branch

---
+++ http_client.c
@@ -0,0 +1,7 @@
+/* Author: Mark Fitzgibbon
+*/
+#include <stdio.h>
+//I haven't used C or C++ in a while, so code may be sloppy
+int main(int argc, char *argv[]){ //Supposedly accepts command line arguments (from stack overflow)
+
+}
