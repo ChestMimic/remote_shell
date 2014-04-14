9cd3afecb72a5f87add9bcc20c3b1ae3946a4de1
ibbolia <mwfitzgibbon@wpi.edu>
2014-04-13 20:53:09 -0400

    Nutella branch will be used for nutella project, a multicast 'movie' streamer.

--- createThread.c
+++ createThread.c
@@ -7,16 +7,13 @@
 #include <sys/types.h>
 #include <arpa/inet.h>
 #include <pthread.h>
-#include <crypt.h>
 #include <sys/socket.h>
 #include <signal.h>
 #include <unistd.h>
+#include <time.h>
 
 #define MAXBUF		1024
 #define TRUE 1
-#define REQUESTOK	"200 OK"
-#define REQUESTBAD	"404 File not found"
-#define PASSWD	"Hello"
 int WorkThread(int fdConn, pid_t pid);
 // ///////////////////////////////////////////////////////////////////////
 // This is the main() code - it is the original thread
@@ -28,21 +25,21 @@ main(int argc, char* argv[]){
 	int listener;
 	int fd;
 	pid_t pid;
-	int numPro = 0;
 	
 	CurrentPriority = getpriority( PRIO_PROCESS, 0 );
 	// Create a new thread that will start executing at location
 	//WorkThread NewThreadID = CreateAThread( (void *)(* WorkThread), &data);
 	
 	// Other main code
-	struct addrinfo /* hints,*/ *servinfo, *p; //info for the server to know about itself
+	struct addrinfo *servinfo, *p; //info for the server to know about itself
 	struct sockaddr_storage their_addr; //connector's socket info
 	socklen_t sin_size;
 	struct sockaddr_in self;
+	
+	
 	// Get the port number via the command line
 	listener = atoi(argv[1]);//grab port number
 	printf("%d\n", listener);
-	//memset(&hints, 0, sizeof hints);
 	
 	/*---Initialize address/port structure---*/
 	bzero(&self, sizeof(self));
@@ -70,142 +67,48 @@ main(int argc, char* argv[]){
 	}//make larger later
 	printf("Listening ...\n");
 	int fdListen = fd;
+	time_t timer;
+	char mostrecenttimestamp[25];
+	struct tm* tm_info;
+	
 	while( TRUE ){
         sin_size = sizeof their_addr;
 		printf("waiting for next accept\n");
 		// Do the accept
-		int fdConn = accept( fdListen, (struct sockaddr *)&their_addr, &sin_size);		
-	//	printf("%s\n", &fdConn);
+		int fdConn = accept( fdListen, (struct sockaddr *)&their_addr, &sin_size);	
+		time(&timer);
+		tm_info = localtime(&timer);
+		strftime(mostrecenttimestamp, 25, "%Y:%m:%d%H:%M:%S", tm_info);
+		puts(mostrecenttimestamp);	
+		printf("Accept made at: %s", mostrecenttimestamp);
+		
 	
 
 		pid = fork();
 		if(pid > 0){
 			WorkThread(fdConn, pid);
 			kill(pid, SIGTERM);//marks child process as dead/zombie to be killed at program exit
-			numPro--;
+			
 		}
-		else{
-			numPro++;
-		}
+		
 		
 	
 	}
 	
 } // End of main
+
+
 // This is the new thread that's created
 //Legacy name, kept for convinience but not actually a thread. Refactor
 int WorkThread( int fdConn, pid_t pid ){
-	//printf("New thread.
-
-	//make input buffer
-	//		printf("%s\n", fdConn);
-
 	char buffer[MAXBUF];
 	char buffer_out[MAXBUF];
 	bzero(buffer, MAXBUF);
 	bzero(buffer_out, MAXBUF);
-	//int size = recv(fdConn, buffer, MAXBUF, 0);
-	//send(fdConn, buffer, recv(fdConn, buffer, MAXBUF, 0), 0);
-	int size=1;
 	
 	
 	
-	size=recv(fdConn, buffer, MAXBUF, 0);
-
-	printf("%s\n", buffer);
-	if(strcmp("mwfitzgibbon", buffer) == 0){
-		
-		//gen unique random number
-		time_t t;
-		char urn1;
-		char urn2;
-		srand((unsigned) time(&t));
-		
-		char urnS[3] = {('A' + rand()%26), ('A' + rand()%26), '\0'};
-		
-		
-		printf("%s\n", urnS);
-		if(	(send(fdConn, urnS, strlen(urnS), 0)) < 0){
-			printf("Error in send\n");
-			//kill self
-		}
-		bzero(buffer, MAXBUF);
-		
-		int rConfirm = 88;
-		while(rConfirm != 0){
-			if ( rConfirm = recv( fdConn, buffer, MAXBUF, 0 ) < 0 ){
-				// Good programming practice will have you generate an error message 
-				// here if the recv()  call fails.
-				perror("Client: recieve");
-			}	
-		
-        
-		
-		}
-
-		printf("%s\n", buffer);
-		char* check =  crypt("Hello", urnS);
-		if(strcmp(check, buffer) == 0){
-			printf("Passwords match\n");
-			
-			bzero(buffer, MAXBUF);
-			
-			
-			rConfirm = 88;
-			while(rConfirm != 0){
-				if ( rConfirm = recv( fdConn, buffer, MAXBUF, 0 ) < 0 ){
-					// Good programming practice will have you generate an error message 
-					// here if the recv()  call fails.
-					perror("Client: recieve");
-				}	
-			}	
-			
-			char line[MAXBUF];
-			strcpy(line, buffer);
-			
-			//parsing influenced from solution found at csl.mtu.edu
-			/*
-			char* token;
-			token = strtok(line, " ");
-			int i = 0;
-			char* path;
-			char* args;
-			printf("ping\n");
-			while(token != NULL){
-				if(i ==0){
-					strcpy(path, token);
-				}
-				else{
-					strcat(args, token);
-					strcat(args, " ");
-				}
-				i++;
-				token = strtok(line, " ");
-			}
-			*/
-			
-			
-			dup2(fdConn, STDOUT_FILENO);
-			dup2(fdConn, STDERR_FILENO);
-			//execl(path, args);
-			system(line);
-			
-		}
-		else{
-			printf("Incorrect password, exiting attempt\n");
-			//kill self
-		}
-	}
-	else{
-		//incorrect username recieved
-		if(	(send(fdConn, "ERR", strlen("ERR"), 0)) < 0){
-			printf("Error in send\n");
-			
-		}
-		//kill self
-	}
 	
-	close(fdConn);
 	
 
 }
--- http_client.c
+++ http_client.c
@@ -11,7 +11,6 @@
 #include <unistd.h>
 #include <stdlib.h>
 #include <sys/time.h>
-#include <crypt.h>
 //I haven't used C or C++ in a while, so code may be sloppy
 /*
 	NOTE TO SELF:
@@ -21,24 +20,23 @@
 
 //code is partially based on tutorial found at http://www.beej.us
 
-#define         BUFFER_SIZE     2000
+#define MAXBUF		1024
 //Main program
 int main(int argc, char *argv[]){ //Supposedly accepts command line arguments (from stack overflow)
 	struct addrinfo hints, *res;
 	struct sockaddr_in sin;
 	int sock;
 	int bytecount;
-	char buffer[BUFFER_SIZE];
-	char request[BUFFER_SIZE];
-	char arg[500], argB[500];
+	char buffer[MAXBUF];
+	char request[MAXBUF];
+	char arg[MAXBUF], argB[MAXBUF];
 	char *fil;
 	
 	memset(&hints, 0, sizeof hints);//ensure struct is empty
 	hints.ai_family = AF_UNSPEC; //Either IPv4 or IPv6
 	hints.ai_socktype = SOCK_STREAM; //TCP stream sockets
 	hints.ai_flags = AI_PASSIVE; //fill in IP for me
-	//printf(argv[1]);
-	//make sure proper number of arguments
+	
 	
 	//split address into host and file
 	int spot;
@@ -46,7 +44,7 @@ int main(int argc, char *argv[]){ //Supposedly accepts command line arguments (f
 	
 	
 	if ( argc < 4 ) {
-        printf( "The program expects two arguments\n" );
+        printf( "The program expects 4 arguments\n" );
         printf( "Browser <URL || IP address> <port number> <command>\n" );
         exit(0);
     }
@@ -60,12 +58,7 @@ int main(int argc, char *argv[]){ //Supposedly accepts command line arguments (f
 	
 	
 	
-	}
-	
-	
-	
-	//printf("%s", argB[strlen(fil)]);
-	
+	}	
 	struct timeval start, end;
 	double time_in_mill_S = 0;
 	double time_in_mill_E = 0;
@@ -75,99 +68,26 @@ int main(int argc, char *argv[]){ //Supposedly accepts command line arguments (f
 
 	
 	sock = socket(res->ai_family, res->ai_socktype, res->ai_protocol);
-	printf("Socket");
 	if(sock == -1){
-		//fprintf(stderr, "getaddrinfo error: %s\n", gai_strerror(status));
 		printf("Socket went bad.\n");
 		exit(1);//kill program
 	}
 	
 	if(connect(sock, res->ai_addr, res->ai_addrlen) == -1){
-	
+		printf("Connect to socket failed. Quitting.\n");
 		exit(1);//something went wrong. Quit
 	}
-	
-	
-	//printf("Enter message to send: ");
-	//int max = 128;
-	char* name = "mwfitzgibbon";
-	//int c;
-	//fgets(name, max, stdin);
-	printf("Username is: %s\n", name);
-	
-	
 	printf("Connected!\n");
+	/*
 	if(	(send(sock, name, strlen(name), 0)) < 0){
 		printf("Error in send\n");
 		exit(1);
-	}
-	/*
-	if(strcmp(buffer, "ERR") == 0){
-		printf("Incorrect username.\n");
-		exit(1);
 	}*/
-	bzero(buffer, BUFFER_SIZE);
 	
-	int rConfirm = 88;
-	while(rConfirm != 0){
-	if ( rConfirm = recv( sock, buffer, BUFFER_SIZE, 0 ) < 0 ){
-	    // Good programming practice will have you generate an error message 
-	    // here if the recv()  call fails.
-			perror("Client: recieve");
-        }
-		
-        
-		
-	}
-	if(strcmp(buffer, "ERR") == 0){//Correct replies will ALWAYS be two characters long
-		printf("Incorrect username.\n");
-		exit(1);
-	}
-	printf( "%s\n", buffer );
-	
-	char* hash = crypt("Hello", buffer); //unsafe in a real world setting
-	printf("%s\n", hash);
-	if(	(send(sock, hash, strlen(hash), 0)) < 0){
+	if(	(send(sock, "hello", strlen("hello"), 0)) < 0){
 		printf("Error in send\n");
 		exit(1);
 	}
-	bzero(buffer, BUFFER_SIZE);
-	
-	//send command
-	//from arg #4
-	printf("%d\n", argc);
-	int i = 3;
-	char cmd[BUFFER_SIZE];
-	while(i < argc){
-		strcat(cmd, argv[i]);
-		strcat(cmd, " ");
-		
-		i++;
-	}
-	printf("%s\n", cmd);
-	
-	if(	(send(sock, cmd, strlen(cmd), 0)) < 0){
-		printf("Error in send\n");
-		exit(1);
-	}
-	
-	
-	while(1){
-		if ( recv( sock, buffer, BUFFER_SIZE, 0 ) <= 0 ){
-			// Good programming practice will have you generate an error message 
-			// here if the recv()  call fails.
-			perror("Client: recieve");
-			break;
-		}
-		else{
-			printf("%s", buffer);
-			bzero(buffer, BUFFER_SIZE);
-		}
-	}
-		
-        
-		
-	
 	
 	
 	//Finish up
